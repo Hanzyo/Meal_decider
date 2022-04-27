c6171a3463d13c0d938ebb51111b048425e28646
YouYou Yu <youyy1225@gmail.com>
2022-04-27 10:24:21 -0500

    wip

--- Makefile
+++ Makefile
@@ -1,21 +1,15 @@
-# Makefile for Writing Make Files Example
- 
-# *****************************************************
-# Variables to control Makefile operation
-% make
-CC = g++
-CFLAGS = -Wall -g
- 
-# ****************************************************
-# Targets needed to bring the executable up to date
- 
-main: main.o Resturants.o
-    $(CC) $(CFLAGS) -o main main.o Resturants.o
- 
-# The main.o target can be written more simply
- 
-main.o: src/main.cpp lib/Resturants.h
-    $(CC) $(CFLAGS) -c src/main.cpp
- 
-Resturants.o: Resturants.h
- 
+
+output.exe: main.o Resturants.o Users.o
+	g++ main.o Resturants.o Users.o -o output.exe
+
+main.o: src/main.cpp
+	g++ -c src/main.cpp
+
+Resturants.o: lib/Resturants.cpp
+	g++ -c lib/Resturants.cpp
+
+Users.o: lib/Users.cpp
+	g++ -c lib/Users.cpp
+
+clean:
+	rm *.o output
--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,71 @@
 #include "../lib/Users.cpp"
 #include <string.h>
 #include <iostream>
+#include <vector.h>
 #include <fstream>
 using namespace std;
+
+Users* rebuild_user(string info){
+    vector<string> v;
+    stringstream ss(info);
+    while (ss.good()) {
+        string substr;
+        getline(ss, substr, '-');
+        v.push_back(substr);
+    }
+    Users* newuser = new Users(v[0]);
+    auto it = v.begin();
+    ++it;
+    while(it != v.end()){
+        string Rname = *it; ++it;
+        int weight = (int)*it; ++it;
+        Resturants* res = new Resturants(Rname, weight);
+        newuser->favorites.insert(res);
+    }
+    return newuser;
+}
 
 Users* initialize_user(){
     string name;
     cout << "Please enter your name:" << endl;
-    char input[100];
+    char input[50];
     cin.getline(input, sizeof(input));
     name = input;
-    Users* newuser = new Users(name);
+    string filename("local/data.txt");
+    vector<string> lines;
+    string line;
+    bool found_name = 0;
+    ifstream input_file(filename);
+    if (!input_file.is_open()) {
+        cerr << "Could not open the file - '" << filename << "'" << endl;
+        return EXIT_FAILURE;
+    }
+
+    while (getline(input_file, line)){
+        lines.push_back(line);
+    }
+    int idx;
+    for(idx = 0; idx < lines.size(); ++idx){
+        char cmpname[50];
+        for(int j = 0; j < strlen(lines[idx]); ++j){
+            if(c != "-"){
+                cmpname[j] = lines[idx][j];
+            }
+        }
+        if(strcmp(name, cmpname) == 0){
+            found_name = 1;
+            break;
+        }
+    }
+    input_file.close();
+    Users* newuser;
+    if(found_name){
+        newuser = rebuild_user(lines[idx]);
+        cout << "Welcome back, " << newuser->getName() << " !" << endl;
+    }
+    else 
+        newuser = new Users(name);
+    
     do{
         cout << "Enter your favorite resturants: (no space bwteen letters, enter finish to end input)" << endl;
         string rest;
@@ -28,20 +83,32 @@ Users* initialize_user(){
     return newuser;
 }
 
-void login_for_user(){
-    Users* loginUser = initialize_user();
+// return 1 if file exists, 0 otherwise
+bool is_file_exist(const char *fileName){
+    std::ifstream infile(fileName);
+    return infile.good();
+}
+
+void save_user_info(Users* user){
     ofstream data;
-    data.open("local/data.txt");
-    data << "Hello, " << loginUser->getName() << endl;
+    data.open("local/data.txt", ios::app);
+    data << user->getName() << "-";
+    for(auto i : user->favorites){
+        data << i->getName() << "-" << i->getWeight() << "-";
+    }
+    data << endl;
     data.close(); 
 }
 
 int main(){
+
     Users* user = initialize_user();
+    save_user_info(user);
     cout << user->getName() << "'s favorite resturants are: " ;
     for(auto i : user->favorites){
         cout << i->getName() << " ";
     }
     cout << endl;
-    
+    return 0;
+
 }
